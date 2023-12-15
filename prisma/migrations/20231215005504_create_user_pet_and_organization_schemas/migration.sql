6bf60f80b91cdbb1075442a53aaffd34d63011ed
Erick Rocha <erick.r.souza@cogna.com.br>
2023-12-14 21:55:57 -0300

    feat: create prisma schemas

---
+++ prisma/migrations/20231215005504_create_user_pet_and_organization_schemas/migration.sql
@@ -0,0 +1,58 @@
+-- CreateEnum
+CREATE TYPE "Age" AS ENUM ('BABY', 'ADULT', 'SENIOR');
+
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('TINY', 'MEDIUM', 'LARGE');
+
+-- CreateEnum
+CREATE TYPE "Energy" AS ENUM ('LOW', 'MODERATE', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "Idependence_level" AS ENUM ('DEPENDENT', 'INDEPENDENT', 'SELF_SUFFICIENT');
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "species" TEXT NOT NULL,
+    "race" TEXT,
+    "name" TEXT NOT NULL,
+    "about" TEXT,
+    "age" "Age" NOT NULL,
+    "size" "Size" NOT NULL,
+    "energy" "Energy" NOT NULL,
+    "independence_level" "Idependence_level" NOT NULL,
+    "requirements" TEXT[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "organization_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "organizations" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "manager_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "CEP" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
