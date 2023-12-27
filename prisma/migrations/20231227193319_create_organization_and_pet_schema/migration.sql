071627ee3f19af1c6e4d386d85b75d8acde3d619
Erick Rocha <erick.r.souza@cogna.com.br>
2023-12-27 16:33:42 -0300

    chore: remove user schema

--- prisma/migrations/20231227193319_create_organization_and_pet_schema/migration.sql
+++ prisma/migrations/20231227193319_create_organization_and_pet_schema/migration.sql
@@ -44,16 +44,5 @@ CREATE TABLE "organizations" (
     CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "users" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password_hash" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
-
 -- AddForeignKey
 ALTER TABLE "pets" ADD CONSTRAINT "pets_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
